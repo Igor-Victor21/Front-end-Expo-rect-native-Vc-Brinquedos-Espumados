7d73bcf15a225b07436cb6fb54182f942ce26792
Igor Victor <igor.victorcontato@gmail.com>
2025-06-30 17:11:02 -0300

    Conectando o BackEnd com o front na parte dos usuários

--- Vc-Brinquedos-Espumados/app/user.tsx
+++ Vc-Brinquedos-Espumados/app/user.tsx
@@ -1,54 +1,85 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { apiVcEspumados } from '@/api/apiVcEspumados';
+import { useEffect, useState } from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 import Nav from '../components/nav-bar';
 
- const User = ({fullName, email, password, CPF, socialReason, StateRegistration, CNPJ, CEP, UF, city, neighborhood, road, numberHouse, complement, numberPhone, dateOfBirth} : 
-    {fullName: string, email: string, password: string, CPF: string, socialReason: string, StateRegistration: string, CNPJ: string, CEP: string, UF: string, city: string, neighborhood: string, 
-        road: string, numberHouse: number, complement: string, numberPhone: string, dateOfBirth: string}) => {
+export default function User() {
+  const [users, setUsers] = useState([]);
+
+  //fazendo a requisição get para mostrar todos os usuários
+  useEffect(() => {
+    apiVcEspumados.get('/users')
+      .then(response => {
+        setUsers(response.data);
+      })
+      .catch(error => {
+        console.error('Erro ao buscar usuários:', error);
+      })
+  }, []);
+
+  //Renderizando os usuarios 
+  const renderItem = ({ item: user }) => (
+    <View style={styles.itemList}>
+      <Text>Nome Completo: {user.fullName}</Text>
+      <Text>E-mail: {user.email}</Text>
+      <Text>Senha: {user.password}</Text>
+      <Text>CPF: {user.cpf}</Text>
+      <Text>Razão Social: {user.socialReason}</Text>
+      <Text>Registro Estadual: {user.stateRegistration}</Text>
+      <Text>CNPJ: {user.cnpj}</Text>
+      <Text>CEP: {user.cep}</Text>
+      <Text>Estado: {user.uf}</Text>
+      <Text>Cidade: {user.city}</Text>
+      <Text>Bairro: {user.neighborhood}</Text>
+      <Text>Rua: {user.road}</Text>
+      <Text>Número da Residência: {user.numberHouse}</Text>
+      <Text>Complemento: {user.complement}</Text>
+      <Text>Telefone: {user.numberPhone}</Text>
+      <Text>Data de nascimento: {user.dateOfBirth}</Text>
+    </View>
+  );
 
   return (
-      <>
-      <Nav/>
-      <View style={styles.itemList}>
-            <Text>{fullName}</Text>
-            <Text>{email}</Text>
-            <Text>{password}</Text>
-            <Text>{CPF}</Text>
-            <Text>{socialReason}</Text>
-            <Text>{StateRegistration}</Text>
-            <Text>{CNPJ}</Text>
-            <Text>{CEP}</Text>
-            <Text>{UF}</Text>
-            <Text>{city}</Text>
-            <Text>{neighborhood}</Text>
-            <Text>{road}</Text>
-            <Text>{numberHouse}</Text>
-            <Text>{complement}</Text>
-            <Text>{numberPhone}</Text>
-            <Text>{dateOfBirth}</Text>
-        </View>
-      </>
-
+    <>
+    
+      {/* Envelopando os dadados dos usuários*/}
+      <View style={styles.wrapPage}>
+        <Text style={styles.titleName}>Usuários Cadastrados</Text>
+        {/* listando os usuários e renderizando todos os dados dos usuários */}
+        <FlatList
+          data={users}
+          renderItem={renderItem}
+          keyExtractor={(item, index) => index.toString()}
+        />
+      </View>
+	{/* Chamando a Nav-bar */}
+      <Nav />
+    </>
   );
 }
 
-export default User;
-
+// css
 const styles = StyleSheet.create({
-  itemList: {
-    display: 'flex',
-    flexDirection: 'row',
+  wrapPage: {
+    flex: 1,
     alignItems: 'center',
-    justifyContent: 'space-between',
-    backgroundColor: '#4168a4d4',
-    margin: 10,
-    height: 80,
-    paddingHorizontal: 10,
-    color: 'black',
+    justifyContent: 'center',
+    paddingTop: 10,
   },
-
-  imageStyle: {
-    width: 50,
-    height: 50,
-    zIndex: 999,
+  titleName: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  flatList: {
+    paddingHorizontal: 10,
+    paddingBottom: 20,
+  },
+  itemList: {
+    backgroundColor: '#A7C7E7',
+    borderRadius: 10,
+    padding: 15,
+    marginVertical: 8,
+    alignSelf: 'stretch',
   },
 });
